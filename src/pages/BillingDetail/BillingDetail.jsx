4e3a2b39714df4f250e1330152cdca6fd71db48b
João Pedro Venturini Sales <jpventurinisales@gmail.com>
2025-07-07 14:24:56 -0300

    feat: Finishing Billing Detail

--- src/pages/BillingDetail/BillingDetail.jsx
+++ src/pages/BillingDetail/BillingDetail.jsx
@@ -15,115 +15,143 @@ import {
   ProductPrice,
   FullPriceArea,
   PriceContainer,
+  PageContainer,
+  SelectRadioArea,
+  RadioArea,
+  RadioInput,
+  RadioLabel,
+  CupomTotalArea,
+  CupomInputArea,
+  CupomInput,
+  BuyButtonArea,
 } from './styles';
 import IMG from '../../assets/images/PhoneImage.png';
+import Button from '../../components/Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaypal } from '@fortawesome/free-brands-svg-icons';
 
 export const BillingPage = () => {
   return (
     <PageStyle>
-      <PageTitleArea>
-        <PageTitle>Informações de Cobrança</PageTitle>
-      </PageTitleArea>
-      <BillingArea>
-        <InputArea>
-          <InputTotalArea>
-            <p>Nome</p>
-            <StyledInput>
-              <input type="text" />
-            </StyledInput>
-          </InputTotalArea>
-          <InputTotalArea>
-            <p>Nome da Empresa</p>
-            <StyledInput>
-              <input type="text" />
-            </StyledInput>
-          </InputTotalArea>
-          <InputTotalArea>
-            <p>Endereço</p>
-            <StyledInput>
-              <input type="text" />
-            </StyledInput>
-          </InputTotalArea>
-          <InputTotalArea>
-            <p>Apartamento, andar, casa, etc. (Opcional)</p>
-            <StyledInput>
-              <input type="text" />
-            </StyledInput>
-          </InputTotalArea>
-          <InputTotalArea>
-            <p>Cidade</p>
-            <StyledInput>
-              <input type="text" />
-            </StyledInput>
-          </InputTotalArea>
-          <InputTotalArea>
-            <p>Telefone</p>
-            <StyledInput>
-              <input type="text" />
-            </StyledInput>
-          </InputTotalArea>
-          <InputTotalArea>
-            <p>Email</p>
-            <StyledInput>
-              <input type="text" />
-            </StyledInput>
-          </InputTotalArea>
-        </InputArea>
-        <ProductArea>
-          <ProductsDetail>
-            <ProductDetailArea>
-              <ProductDetailInfo>
-                <ProductImage src={IMG} />
-                <ProductName>Celular Teste 123</ProductName>
-              </ProductDetailInfo>
-              <ProductPrice>
-                <span>R$</span>650
-              </ProductPrice>
-            </ProductDetailArea>
-            <ProductDetailArea>
-              <ProductDetailInfo>
-                <ProductImage src={IMG} />
-                <ProductName>Celular Teste 123</ProductName>
-              </ProductDetailInfo>
-              <ProductPrice>
-                <span>R$</span>650
-              </ProductPrice>
-            </ProductDetailArea>
-            <FullPriceArea>
-              <PriceContainer>
-                <div>Subtotal</div>
-                <div>
+      <PageContainer>
+        <PageTitleArea>
+          <PageTitle>Informações de Cobrança</PageTitle>
+        </PageTitleArea>
+        <BillingArea>
+          <InputArea>
+            <InputTotalArea>
+              <p>Nome</p>
+              <StyledInput>
+                <input type="text" />
+              </StyledInput>
+            </InputTotalArea>
+            <InputTotalArea>
+              <p>Nome da Empresa</p>
+              <StyledInput>
+                <input type="text" />
+              </StyledInput>
+            </InputTotalArea>
+            <InputTotalArea>
+              <p>Endereço</p>
+              <StyledInput>
+                <input type="text" />
+              </StyledInput>
+            </InputTotalArea>
+            <InputTotalArea>
+              <p>Apartamento, andar, casa, etc. (Opcional)</p>
+              <StyledInput>
+                <input type="text" />
+              </StyledInput>
+            </InputTotalArea>
+            <InputTotalArea>
+              <p>Cidade</p>
+              <StyledInput>
+                <input type="text" />
+              </StyledInput>
+            </InputTotalArea>
+            <InputTotalArea>
+              <p>Telefone</p>
+              <StyledInput>
+                <input type="text" />
+              </StyledInput>
+            </InputTotalArea>
+            <InputTotalArea>
+              <p>Email</p>
+              <StyledInput>
+                <input type="text" />
+              </StyledInput>
+            </InputTotalArea>
+          </InputArea>
+          <ProductArea>
+            <ProductsDetail>
+              <ProductDetailArea>
+                <ProductDetailInfo>
+                  <ProductImage src={IMG} />
+                  <ProductName>Celular Teste 123</ProductName>
+                </ProductDetailInfo>
+                <ProductPrice>
                   <span>R$</span>650
-                </div>
-              </PriceContainer>
-              <PriceContainer>
-                <div>Frete</div>
-                <div>
-                  <span>R$</span>20
-                </div>
-              </PriceContainer>
-              <PriceContainer bordertop="none">
-                <div>Total</div>
-                <div>
-                  <span>R$</span>670
-                </div>
-              </PriceContainer>
-            </FullPriceArea>
-          </ProductsDetail>
-          <div>
-            <div>
-              <input type="radio" />
-            </div>
-            <div>aa</div>
-          </div>
-          <div>
-            <div>
-              <input type="radio" />
-            </div>
-            <div>aa</div>
-          </div>
-        </ProductArea>
-      </BillingArea>
+                </ProductPrice>
+              </ProductDetailArea>
+              <ProductDetailArea>
+                <ProductDetailInfo>
+                  <ProductImage src={IMG} />
+                  <ProductName>Celular Teste 123</ProductName>
+                </ProductDetailInfo>
+                <ProductPrice>
+                  <span>R$</span>650
+                </ProductPrice>
+              </ProductDetailArea>
+              <FullPriceArea>
+                <PriceContainer>
+                  <div>Subtotal</div>
+                  <div>
+                    <span>R$</span>650
+                  </div>
+                </PriceContainer>
+                <PriceContainer>
+                  <div>Frete</div>
+                  <div>
+                    <span>R$</span>20
+                  </div>
+                </PriceContainer>
+                <PriceContainer bordertop="none">
+                  <div>Total</div>
+                  <div>
+                    <span>R$</span>670
+                  </div>
+                </PriceContainer>
+              </FullPriceArea>
+            </ProductsDetail>
+            <SelectRadioArea>
+              <RadioArea>
+                <RadioInput id="credit" name="PaymentType" type="radio" />
+                <RadioLabel htmlFor="credit">Cartão de Crédito</RadioLabel>
+              </RadioArea>
+              <FontAwesomeIcon
+                size="2xl"
+                icon={faPaypal}
+                style={{ color: '#2c8ab7' }}
+              />
+            </SelectRadioArea>
+            <SelectRadioArea>
+              <RadioArea>
+                <RadioInput id="cash" name="PaymentType" type="radio" />
+                <RadioLabel htmlFor="cash">Dinheiro na entrega</RadioLabel>
+              </RadioArea>
+            </SelectRadioArea>
+            <CupomTotalArea>
+              <CupomInputArea>
+                <CupomInput type="text" placeholder="Código de Desconto" />
+              </CupomInputArea>
+              <Button buttonText="Aplicar Cupom" />
+            </CupomTotalArea>
+            <BuyButtonArea>
+              <Button buttonText="Finalizar Compra" />
+            </BuyButtonArea>
+          </ProductArea>
+        </BillingArea>
+      </PageContainer>
     </PageStyle>
   );
 };
--- src/pages/BillingDetail/styles.js
+++ src/pages/BillingDetail/styles.js
@@ -9,9 +9,12 @@ export const PageStyle = styled.section`
     overflow: hidden;
 `
 
+export const PageContainer = styled.div`
+
+`
+
 export const PageTitleArea = styled.div`
     display: flex;
-    align-items: flex-start;
     font-size: 14px;
 `
 
@@ -23,6 +26,7 @@ export const PageTitle = styled.h1`
 export const BillingArea = styled.div`
     margin-top: 50px;
     display: flex;
+    gap: 100px;
 `
 
 export const InputArea = styled.div`
@@ -105,3 +109,95 @@ export const PriceContainer = styled.div`
     padding-bottom: 15px;
     border-bottom: ${(props) => props.bordertop || '2px solid #999999'};
 `
+
+export const SelectRadioArea = styled.div`
+    display: flex;
+    align-items: center;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    justify-content: space-between;
+
+`
+
+export const RadioArea = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+`
+
+export const RadioInput = styled.input.attrs({ type: "radio" })`
+     display: none;
+`
+export const RadioLabel = styled.label`
+  position: relative;
+  padding-left: 30px;
+  cursor: pointer;
+  font-size: 16px;
+  user-select: none;
+
+  &::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 20px;
+    height: 20px;
+    border: 2px solid #333;
+    border-radius: 50%;
+    background-color: #fff;
+    box-sizing: border-box;
+    transition: all 0.2s ease;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 4px;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 12px; 
+    height: 12px;
+    background-color: black; 
+    border-radius: 50%;
+    opacity: 0;
+    transition: all 0.2s ease;
+  }
+
+  input:checked + &::after {
+    opacity: 1;
+  }
+`
+export const CupomTotalArea = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    margin-top: 15px;
+`
+export const CupomInputArea = styled.div`
+    border: 1.5px solid black;
+    border-radius: 5px;
+
+`
+export const CupomInput = styled.input`
+    margin: 5px;
+    height: 40px;
+    padding: 0px 4px ;
+    font-size: 15px;
+    text-transform: uppercase;
+      font-family: 'Inter',Arial, Helvetica, sans-serif;
+        background-color: transparent;
+        outline: none;
+        border: none;
+        text-align: center;
+
+    &::placeholder {
+        text-transform: none;
+    }
+`
+
+export const BuyButtonArea = styled.div`
+    margin: auto;
+    margin-top: 30px;
+`
+;
